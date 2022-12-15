ffe1c4b46c2d126751971a38352038bf703406c9
ducky <gyozoke2007@gmail.com>
2022-12-15 11:43:21 +0200

    Fixed another idiotic mistake

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@
 mkdir -p ~/.local/bin/tblvis/
 mkdir -p ~/.local/share/applications/
 cp ./install/run.sh ~/.local/bin/tblvis/
-cp ./install/tblvis ~/.local/bin/tblvis/
+cp ./tblvis ~/.local/bin/tblvis/
 sed "s,~,${HOME}," ./install/desktop-visualizer.desktop > $HOME/.local/share/applications/desktop-visualizer.desktop
