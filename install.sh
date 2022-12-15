66b19f13967a64e703295fe7a3d3ae8855959e80
ducky <gyozoke2007@gmail.com>
2022-12-15 11:42:34 +0200

    Made the installer actually work

--- install.sh
+++ install.sh
@@ -6,5 +6,4 @@ mkdir -p ~/.local/bin/tblvis/
 mkdir -p ~/.local/share/applications/
 cp ./install/run.sh ~/.local/bin/tblvis/
 cp ./install/tblvis ~/.local/bin/tblvis/
-cp ./install/desktop-visualizer.desktop ~/.local/share/applications/
-
+sed "s,~,${HOME}," ./install/desktop-visualizer.desktop > $HOME/.local/share/applications/desktop-visualizer.desktop
