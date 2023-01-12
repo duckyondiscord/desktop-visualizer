510637f6fb841d131ebc7154cf8c95329392fd32
ducky <94759537+duckyondiscord@users.noreply.github.com>
2023-01-12 19:49:55 +0200

    Added a script for automatically making releases

---
+++ mkrelease.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script used for making releases
+
+# Make project
+cmake .
+make -j$(nproc --all)
+
+# Create release tarball
+mkdir -p desktop-visualizer/install
+cp tblvis desktop-visualizer/
+cp -r install/* desktop-visualizer/install
+cp install.sh desktop-visualizer/
+tar -caf $(uname)-$(uname -m).tar.gz desktop-visualizer/*
