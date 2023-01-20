2d02e3df015996c4ce2f372dbdff54a70cae4dae
ducky <94759537+duckyondiscord@users.noreply.github.com>
2023-01-20 17:48:34 +0200

    Made the installer remove old versions

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ make -j$(nproc --all)
 mkdir -p ~/.local/bin/
 mkdir -p ~/.local/share/applications/
 cp install/deskvis.ini ~/.config/deskvis.ini
+rm -rf ~/.local/bin/tblvis
 cp -f ./tblvis ~/.local/bin
 sed "s,~,${HOME}," ./install/desktop-visualizer.desktop > $HOME/.local/share/applications/desktop-visualizer.desktop
