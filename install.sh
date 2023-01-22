b0a6a684eca1547487f599ec1a7831aa4aadc440
duckyondiscord <duccywastaken@protonmail.com>
2023-01-22 10:11:49 +0200

    Added release tarball to .gitignore and added an option to install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ cp install/deskvis.ini ~/.config/deskvis.ini
 rm -rf ~/.local/bin/tblvis
 cp -f ./tblvis ~/.local/bin
 sed "s,~,${HOME}," ./install/desktop-visualizer.desktop > $HOME/.local/share/applications/desktop-visualizer.desktop
+
+echo "Do you want to disable the \"application not responding\" dialog that appears on GNOME sometimes with this app? [y/n]"
+read yesno
+
+if [ $yesno == "y" ]; then
+    gsettings set org.gnome.desktop.wm.preferences auto-raise-delay 0
+
+fi
