58742b4b4052ea3ad54810e261e654ea9dbfc20c
duckyondiscord <duccywastaken@protonmail.com>
2023-02-12 14:32:35 +0200

    Made a couple changes to the mkrelease script

--- mkrelease.sh
+++ mkrelease.sh
@@ -3,13 +3,15 @@
 # Script used for making releases
 
 # Make project
-cmake .
-make -j$(nproc --all)
+echo "Building project..."
+cmake . > /dev/null
+make -j$(nproc --all) > /dev/null
 
 # Create release tarball
+echo "Making release tarball..."
 mkdir -p desktop-visualizer/install
 cp tblvis desktop-visualizer/
 cp -r install/* desktop-visualizer/install
 cp install.sh desktop-visualizer/
-tar -caf $(uname)-$(uname -m).tar.gz desktop-visualizer/*
+tar -cvaf $(uname)-$(uname -m).tar.gz desktop-visualizer/*
 rm -rf desktop-visualizer/
